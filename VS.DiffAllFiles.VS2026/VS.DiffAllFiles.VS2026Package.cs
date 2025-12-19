3f8481f51b75dc113d30ca0f3d56f8ede2aab9ea
deadlydog <deadlydog@hotmail.com>
2025-12-19 11:18:44 -0600

    Add new VS2026 project to solution

---
+++ VS.DiffAllFiles.VS2026/VS.DiffAllFiles.VS2026Package.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using VS_DiffAllFiles;
+
+namespace DansKingdom.VS_DiffAllFiles_VS2026
+{
+	// Assign the VS Guid that matches the VSIX manifest to this package.
+	[Guid(Guids.guidVS_DiffAllFiles_VS2026PkgString)]
+	public sealed class VS_DiffAllFiles_VS2026Package : VS_DiffAllFilesPackage
+	{ }
+}
